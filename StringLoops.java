3b0204ac8d45da94f7efc89df78bdcf4a9108cca
23NateK <n.kimber22@gmail.com>
2023-02-13 11:27:26 -0600

    Finished reverseCharacters

--- StringLoops.java
+++ StringLoops.java
@@ -1,10 +1,17 @@
-public class Loops {
+public class StringLoops {
 
   // static methods go here
-
+public static String reverseCharacters(String a){
+  String newstring = "";
+  int len = a.length();
+  for (int n=0;n<len;n++){
+  newstring = newstring+a.substring(len-n-1,len-n);
+  }
+return newstring;
+}
   public static void main(String[] args) {
 
     // testing of static methods goes here
-
+    System.out.println(reverseCharacters("academy"));
   }
 }
