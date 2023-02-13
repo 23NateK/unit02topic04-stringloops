169c959f8d249675707e50f209a5002a63987bd5
23NateK <n.kimber22@gmail.com>
2023-02-13 11:56:36 -0600

    Finished indexesOfAll

--- StringLoops.java
+++ StringLoops.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+
+import javax.swing.text.StyleConstants;
+
 public class StringLoops {
 
   // static methods go here
@@ -9,9 +13,25 @@ public static String reverseCharacters(String a){
   }
 return newstring;
 }
+public static int[] indexesOfAll(String a,String letter){
+int [] values = new int[0];
+int len = a.length();
+for (int n=1;n<len;n++){
+  if (a.substring(n-1, n).equals(letter)){
+      int[] copyList = new int[values.length+1];
+      for(int x = 0; x< values.length; x++){
+        copyList[x] = values[x];
+      }
+      copyList[copyList.length-1] = n;
+      values = copyList;
+  }
+}
+return values;
+}
   public static void main(String[] args) {
 
     // testing of static methods goes here
-    System.out.println(reverseCharacters("academy"));
+    //System.out.println(reverseCharacters("academy"));
+    System.out.println(Arrays.toString(indexesOfAll("attack","a")));
   }
 }
